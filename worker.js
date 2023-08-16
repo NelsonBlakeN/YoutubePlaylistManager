528eefffd3984d035bfe1a7069dce4e7d0ec7dac
Blake Nelson <mail@blakenelson.me>
2023-08-16 11:03:01 -0700

    Complete worker by having it fetch the tokens using the auth URL and save them to persistent storage

--- worker.js
+++ worker.js
@@ -1,6 +1,7 @@
 const { google } = require('googleapis')
 const { OAuth2Client } = require('google-auth-library')
 const { Storage } = require('@google-cloud/storage')
+const readline = require('readline')
 
 const { clientCredentials, bucketName } = require("./credentials")
 
@@ -19,15 +20,21 @@ function generateAuthUrl() {
             'https://www.googleapis.com/auth/youtube.readonly',
             'https://www.googleapis.com/auth/youtube'
         ],
-        access_type: 'offline'
+        access_type: 'offline',
+        // prompt: 'consent', // Only required if needing to force OAuth to regenerate a refresh token
     })
+
     return authUrl;
 }
 
 async function getAccessTokenFromCode(code) {
     const { tokens } = await oAuth2Client.getToken(code)
     console.log(tokens)
-    return tokens.access_token
+
+    return {
+      accessToken: tokens.access_token,
+      refreshToken: tokens.refresh_token
+    }
 }
 
 async function getRecentSubscribedVideos(apiKey, accessToken) {
@@ -42,7 +49,7 @@ async function getRecentSubscribedVideos(apiKey, accessToken) {
             part: 'snippet',
             mine: true,
             maxResults: 50,
-            access_token: accessToken
+            access_token: accessToken,
         });
 
         const channelIds = subscriptions.data.items.map(item => item.snippet.resourceId.channelId);
@@ -67,67 +74,107 @@ async function getRecentSubscribedVideos(apiKey, accessToken) {
     }
 }
 
-const apiKey = require("./credentials")
+async function getToken() {
+  // Get data from storage
+  let tokens;
+  try {
+    // TODO: Test what happens if the file is not there, does this throw an error? Return null? Something else?
+    tokens = JSON.parse(await bucket.file('token-data').download())
+    console.log(tokens)
+  } catch(e) {
+    console.log(e)
+  }
 
+  tokens = undefined // temp
 
-// Put below logic into a function that generates the URL, asks for the code,
-// then pushes it to the cloud storage
-// This function should also do the logic of checking the storage location
-// If the storage location is empty, generate the URL, get the code, save to cloud storage, then return token
-// If location is not empty, create OAuth client using the credentials and check validity
-// If the credentials are expired, use the refresh token to get new tokens and save to cloud storage, then return
-// Otherwise, just return the token
-// Instead of returning the token,
-const authUrl = generateAuthUrl();
-console.log('Please visit the following URL to authorize the application:');
-console.log(authUrl)
+  if (tokens) { // Or whatever condition is needed for if file/contents are empty
+    // Flow to generate and return OAuth client using the refresh token goes here
+  }
+  else {
+    // If the storage location is empty, generate the URL, get the code, save to cloud storage, then return token
+    //// TODO: Test what happens if file data is empty (file exists but has not data)
+    const authUrl = generateAuthUrl()
+    console.log('Please visit the following URL to authorize the application:');
+    console.log(authUrl)
 
-const readline = require('readline')
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
 
-rl.question('Enter the authorization code from the URL: ', async (code) => {
-    rl.close();
+    // TODO: How do I return something from here and guarantee that it's ready when I need it?
+    const fetchTokenUsingAuthUrl = () => {
+      return new Promise((resolve, reject) => {
+        rl.question('Enter the authorization code from the URL: ', async (code) => {
+          rl.close()
 
-    let contents = await bucket.file('token-data').download()
-    console.log(JSON.parse(contents))
+          const { accessToken, refreshToken } = await getAccessTokenFromCode(code)
 
-    try {
-      contents = {
-        'message': 'Test file2'
-      }
-      await bucket.file('token-data').save(JSON.stringify(contents))
-    } catch(e) {
-      console.log(e)
+          // Save code
+          try {
+            await bucket.file('token-data').save(JSON.stringify({
+              'access_token': accessToken,
+              'refresh_token': refreshToken
+            }))
+          } catch(e) {
+            console.log("Data save failed with error:", e)
+          }
+
+          resolve(accessToken)
+        })
+      })
     }
 
-    const accessToken = await getAccessTokenFromCode(code);
-    if (accessToken) {
-        getRecentSubscribedVideos(apiKey, accessToken)
-            .then(videos => {
-                if (videos) {
-                    console.log('Recent subscribed videos:');
-                    videos.forEach(video => {
-                        console.log(video.snippet.title)
-                    })
-                }
-            })
-    }
+    // Confirm that this returns the value correctly, then update the callback to create the
+    // OAuth client instead
+    const result = await fetchTokenUsingAuthUrl()
+    console.log("Question result:")
+    console.log(result)
+    return result
+  }
+}
+
+async function main() {
+  await getToken()
+}
+
+main().then(() => {
+  console.log("Completed")
+})
+.catch(e => {
+  console.log(e)
+  console.log("Completed with errors")
 })
 
-// Have a storage location for access token and refresh token
+// rl.question('Enter the authorization code from the URL: ', async (code) => {
+//     rl.close();
 
-// If they are empty, have the user get and provide the access code manually
-//// access token and refresh token are then fetched and stored in storage location
+//     await getToken()
+//     return
 
-// If they are not empty, create the OAuth client using the credentials and check if they are expired
-// If they are expired, use the refresh token to get a new access token and save to location
+//     let contents = await bucket.file('token-data').download()
+//     console.log(JSON.parse(contents))
 
-// From there, use the api key and access token to fetch and modify data
+//     try {
+//       contents = {
+//         'message': 'Test file2'
+//       }
+//       await bucket.file('token-data').save(JSON.stringify(contents))
+//     } catch(e) {
+//       console.log(e)
+//     }
 
-// This way, on the first execution the user will have to provide the information. After that,
-// the function should be able to pull the tokens from storage and either use them or refresh and
-// use them
-//// This assumes that the refresh token will never expire
+//     const accessToken = await getAccessTokenFromCode(code);
+//     if (accessToken) {
+//         getRecentSubscribedVideos(apiKey, accessToken)
+//             .then(videos => {
+//               console.log("Videos:", videos)
+//                 if (videos) {
+//                     console.log('Recent subscribed videos:');
+//                     videos.forEach(video => {
+//                         console.log(video.snippet.title)
+//                     })
+//                 }
+//             })
+//     }
+// })
